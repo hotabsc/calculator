6ea1238590b12c42f7f61545ae6713f93ce16549
merdoc73ru <dzavarzin91@gmail.com>
2016-06-30 23:51:26 +0400

    prepare issue for unit testing

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculator
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        Calculator calculatorProcessor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            calculatorProcessor = new Calculator();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            calculatorProcessor.add(number);
+            tbx_Result.Text = calculatorProcessor.GetResult().ToString();
+        }
+
+        private void subButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            calculatorProcessor.substract(number);
+            tbx_Result.Text = calculatorProcessor.GetResult().ToString();
+        }
+
+        private void multButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            calculatorProcessor.multiply(number);
+            tbx_Result.Text = calculatorProcessor.GetResult().ToString();
+        }
+
+        private void divButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            calculatorProcessor.divide(number);
+            tbx_Result.Text = calculatorProcessor.GetResult().ToString();
+        }
+    }
+
+    public class Calculator {
+        private long currentResult = 0;
+
+        public long GetResult() {
+            return currentResult;
+        }
+
+        public void add(long arg) {
+            //TODO: add test
+            currentResult += arg;
+        }
+
+        public void substract(long arg) {
+            //TODO: add test
+            currentResult -= arg;
+        }
+
+        public void multiply(long arg) {
+            //TODO: add test
+            //TODO: implement
+        }
+
+        public void divide(long arg) {
+            //TODO: add test
+            //TODO: implement
+        }
+    }
 }
